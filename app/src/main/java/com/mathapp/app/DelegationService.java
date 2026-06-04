505a566c35ceaa4b86406a0a971010f26780f95f
Dalami <diegoalami@gmail.com>
2026-06-04 12:44:02 -0300

    apk

---
+++ app/src/main/java/com/mathapp/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.mathapp.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
