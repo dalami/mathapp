86667bc654736814638f45a7ca1a5ede3f084f68
Dalami <diegoalami@gmail.com>
2026-06-23 09:09:40 -0300

    redirect

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,5 +1,19 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
 
 export default function ResetPasswordPage() {
-  redirect("/");
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const tokenHash = params.get("token_hash");
+    const type = params.get("type");
+
+    if (tokenHash && type === "recovery") {
+      window.location.href = `mathapp://reset-password?token_hash=${tokenHash}&type=recovery`;
+    } else {
+      window.location.href = "/";
+    }
+  }, []);
+
+  return null;
 }
